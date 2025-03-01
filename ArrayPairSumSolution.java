dd900e0e20634dab8fb47416b47908b9cf8080c4
Srinivas Jadcharla <jdj2ee@gmail.com>
2025-03-01 15:34:00 -0700

    Array-4

---
+++ ArrayPairSumSolution.java
@@ -0,0 +1,14 @@
+class ArrayPairSumSolution {
+    //TC - O(nlogn) SC - O(1)
+    public int arrayPairSum(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        Arrays.sort(nums);
+        int total = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            total += nums[i];
+        }
+        return total;
+    }
+}
